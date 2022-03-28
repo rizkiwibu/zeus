ced630afe3e581ee55225a7686723a095ccaf260
Rizkikun <101055276+Rizkikun@users.noreply.github.com>
2022-03-28 14:56:51 +0700

    config.js

--- config.js
+++ config.js
@@ -15,10 +15,10 @@ gc1 = ''
 gc2 = ''
 gc3 = ''
 global.linkGC = ['https://chat.whatsapp.com/HJ9pD2XJ8vq7whuAYOvT1Y', ''] // ganti jadi group lu
-global.owner = ['6289625556161','62896255561613'] // Put your number here //owner eval
-global.kontak = ['6289625556161','62896255561613'] //Ketika ada yang ngetik #owner
+global.owner = ['6283817161861','6283817161861'] // Put your number here //owner eval
+global.kontak = ['6283817161861','6283817161861'] //Ketika ada yang ngetik #owner
 global.mods = ['0'] // Want some help?
-global.prems = ['6289625556161','601164215501','60166408295','601161197507','601156232697','601172962735','60182387590','60109211153','6281391552752'] // Premium user has unlimited limit
+global.prems = ['6283817161861','601164215501','60166408295','601161197507','601156232697','601172962735','60182387590','60109211153','6283817161861'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   rey: 'https://server-api-rey.herokuapp.com',
@@ -47,23 +47,23 @@ zekskey = 'apivinz'
 xteamkey = 'Dawnfrostkey'
 //xteam        MIMINETBOT
 namaig = 'GAK ADA :)'
-namagithub = 'https://github.com/MASTERMAN200'
+namagithub = 'https://github.com/rizkiwibu'
 kasihcaption = `Nih kak`
-namakontak1 = 'Pilar Owner Bot'
-namakontak2 = 'Caca Admin Bot'
+namakontak1 = 'rizki Owner Bot'
+namakontak2 = 'irfan owner Bot'
 
 //kasihcaption = `Nih Kak`
 
 // Sticker WM
-global.packname = 'Zeus Botz' // ganti aja
-global.author = 'Zeus Botz' // ganti aja
+global.packname = 'iky Botz' // ganti aja
+global.author = 'rizkiwibu' // ganti aja
 
 //yyy
-bc = 'ZeusBotz' //ZeusBotz Broadcast
-footer = '\n© ZeusBotz'
-namabot = 'ZEUS BOTZ'
-namalu = 'PILAR'
-watermark = 'ZeusBotz' //ganti aja
+bc = 'ikyBotz' //ikybotz Broadcast
+footer = '\n© ikyBotz'
+namabot = 'IKY BOTZ'
+namalu = 'rizki'
+watermark = 'rizkiwibu' //ganti aja
 
 // 
 wait = '_*Tunggu Sebentar...*_'
